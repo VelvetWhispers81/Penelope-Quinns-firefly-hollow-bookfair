2f8b3fccc7c7c7862a686aec2fd37399a6a71afb
VelvetWhispers81 <mummabull81@gmail.com>
2026-07-21 08:32:32 -0500

    Add Footer component with copyright information

---
+++ footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer style={{ textAlign: "center", padding: "2rem", color: "var(--moonstone)" }}>
+      <p>© 2026 Penelope Quinn’s Firefly Hollow Book Fair</p>
+    </footer>
+  )
+}
